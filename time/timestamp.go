362b4e5dd5b42c02c2b8ebed2640aa7e158c3e7c
songxuexian <s.xue.xian.c@gmail.com>
2021-05-28 14:28:02 +0800

    add time pkg

---
+++ time/timestamp.go
@@ -0,0 +1,61 @@
+package time
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+
+	"gopkg.in/mgo.v2/bson"
+)
+
+// Timestamp time.Time
+type Timestamp time.Time
+
+// Unix get the unix time
+func (t *Timestamp) Unix() int64 {
+	tt := time.Time(*t)
+	if tt.IsZero() {
+		return 0
+	}
+	return tt.Unix()
+}
+
+// MarshalJSON transfer timestamp to byte
+func (t *Timestamp) MarshalJSON() ([]byte, error) {
+	ts := time.Time(*t).Unix()
+	stamp := fmt.Sprint(ts)
+	return []byte(stamp), nil
+}
+
+// UnmarshalJSON transfer byte to timestamp
+func (t *Timestamp) UnmarshalJSON(b []byte) error {
+	ts, err := strconv.Atoi(string(b))
+	if err != nil {
+		return err
+	}
+
+	*t = Timestamp(time.Unix(int64(ts), 0))
+	return nil
+}
+
+// GetBSON implements bson.Getter
+func (t Timestamp) GetBSON() (interface{}, error) {
+	return struct {
+		Value int64
+	}{
+		Value: time.Time(t).Unix(),
+	}, nil
+}
+
+// SetBSON implements bson.Setter
+func (t *Timestamp) SetBSON(raw bson.Raw) error {
+	decoded := new(struct {
+		Value int64
+	})
+	if err := raw.Unmarshal(decoded); err != nil {
+		return err
+	}
+
+	*t = Timestamp(time.Unix(decoded.Value, 0))
+	return nil
+}
